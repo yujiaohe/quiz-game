84028ba94054aabebe55c4e38ff2f4f2a22ee2bd
yujiaohe <heyujiaohuhu@gmail.com>
2023-09-05 14:43:59 +0200

    Merge branch 'main' of https://github.com/yujiaohe/quiz_game

--- text-based/main.py
+++ text-based/main.py
@@ -2,12 +2,12 @@ from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
 
-question_back = []
+question_bank = []
 for item in question_data:
     new_q = Question(item["text"], item["answer"])
-    question_back.append(new_q)
+    question_bank.append(new_q)
 
-quiz = QuizBrain(question_back)
+quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
